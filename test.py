86dcd1cb9b36e37f1096f869d7a7658ca40a76bf
jabaliSK <62538302+jabaliSK@users.noreply.github.com>
2025-09-09 21:25:25 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+echo 'from airflow.operators.python import PythonVirtualenvOperator
+from datetime import datetime
+ 
+def my_task():
+    from pycentral.base import DNACenter  # import inside the task
+    print("pycentral imported successfully")
+ 
+with DAG(
+    dag_id="test_pycentral",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+ 
+    task = PythonVirtualenvOperator(
+        task_id="use_pycentral",
+        python_callable=my_task,
+        requirements=["pycentral"],
+        system_site_packages=False,
+    )' > arubavenv.py
