3f6e8ae5b2813226eee12f0aaaca2685be8ad038
jabaliSK <62538302+jabaliSK@users.noreply.github.com>
2025-09-15 17:22:37 +0530

    Update test.py

--- dags/test.py
+++ dags/test.py
@@ -1,20 +1,19 @@
-from airflow.operators.python import PythonVirtualenvOperator
-from datetime import datetime
 from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import os
 
-def my_task():
+def print_env_variable():
     print(os.environ)
- 
+
 with DAG(
-    dag_id="test_pycentral",
+    dag_id='print_env_var_dag',
     start_date=datetime(2023, 1, 1),
-    schedule_interval=None,
+    schedule=None,
     catchup=False,
+    tags=['example'],
 ) as dag:
- 
-    task = PythonVirtualenvOperator(
-        task_id="use_pycentral",
-        python_callable=my_task,
-        requirements=["os"],
-        system_site_packages=False,
+    print_var_task = PythonOperator(
+        task_id='print_environment_variable',
+        python_callable=print_env_variable,
     )
